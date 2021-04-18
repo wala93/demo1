3df13fa480e8475abdf4540c69f3e9e25e97bc1a
Walaa Alomari <walaahmad1987@gmail.com>
2021-04-18 04:07:16 +0300

    show done

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.get('/hello',renderHomePage);
 
 app.get('/searches/new',showForm);
 
+app.post('/searches',creatSearch);
 
 
 
@@ -41,7 +42,13 @@ app.get('/searches/new',showForm);
 
 
 
-
+//-----------------------------------------------------------------------
+function Book(info) {
+  this.title = info.title || 'No title available'; // shortcircuit
+  this.author = info.authors || 'No author available';
+  this.description = info.description || 'No description available';
+  this.image = (info.imageLinks) ? info.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+}
 
 
 
@@ -56,4 +63,40 @@ function renderHomePage(req,res){
 function showForm(req,res){
 
   res.render('pages/searches/new');
+}
+
+// function creatSearch (req,res){
+//   let url='https://www.googleapis.com/books/v1/volumes?q=';
+//   console.log(req.body.search);
+//   if (req.body.search[1]==='title'){url += `intitle:${req.body.search[0]}`;}
+//   if (req.body.search[1]==='author'){url += `inauthor:${req.body.search[0]}`;}
+
+//   superagent.get(url).then(
+//     y=>{
+//     //   console.log(y.body.items);
+//       y.body.items.map(resultApi=>{new Book(resultApi.volumeInfo);
+//         // console.log(resultApi.volumeInfo);
+//       });}
+//   ).then(results=>{
+//     console.log(results);
+//     res.render('pages/index',{searchResults:results});
+//   });
+
+
+// }
+
+function creatSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  console.log(request.body);
+  console.log(request.body.search);
+
+  // can we convert this to ternary?
+  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/index', { searchResults: results }))
+    .catch(error => response.status(500).send(`somthing wrong ${error}`));
 }
